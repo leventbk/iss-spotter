fb96d0f63dbb70bcbf074f2f85b92b03d2d3b3a3
Leventbk <leventbkarakus@hotmail.com>
2023-08-04 14:17:33 -0400

    Fetch Flyover Times

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // index.js
 
-// The code below is temporary and can be commented out.
-const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// The code below is temporary and can be commented out.
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
 
-  console.log('It worked! Returned IP:', ip);
+  console.log('It worked! Returned flyover times:', passTimes);
 });
